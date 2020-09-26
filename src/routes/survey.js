a20fb3ed2cb88bca0f4ae7a85cbc5f6ef73d14db
alynevieira <alynealicevieira1@gmail.com>
2020-09-26 14:04:46 -0300

    wip

--- src/routes/survey.js
+++ src/routes/survey.js
@@ -4,11 +4,12 @@ const express = require('express')
 const router = express.Router()
 const debug = require('debug')
 const mongoose = require('mongoose')
+const { Schema } = mongoose;
 
 router.post('/', async (req, res) => {
     try {
         const data = req.body
-        const surveySchema = new mongoose.Schema({}, { strict: false })
+        const surveySchema = new Schema({}, { strict: false })
         const Survey = mongoose.model('Survey', surveySchema)
         const survey = new Survey(data)
 
