61c8882a84b35fa4613a12744e8b092485153ae2
Eduardo Vieira <eduardofelipevieira@gmail.com>
2020-09-28 20:43:34 -0300

    bugfix schema mongoose

--- src/routes/survey.js
+++ src/routes/survey.js
@@ -4,13 +4,11 @@ const express = require('express')
 const router = express.Router()
 const debug = require('debug')('e2l:api')
 const mongoose = require('mongoose')
-const { Schema } = mongoose;
+const Survey = require('../model/survey')
 
 router.post('/', async (req, res) => {
     try {
         const data = req.body
-        const surveySchema = new Schema({}, { strict: false })
-        const Survey = mongoose.model('Survey', surveySchema)
         const survey = new Survey(data)
 
         await survey.save()
@@ -18,7 +16,7 @@ router.post('/', async (req, res) => {
         res.json(survey)
     } catch (error) {
         debug(error)
-        
+
         res.status(500).json(error)
     }
 })
